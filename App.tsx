13318ebabdcb921c869e549591b003e9a1d45881
Abhi25786 <abhi.thakur25786@gmail.com>
2023-03-05 01:04:19 +0530

    about timing ,springand repeat animation

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import AboutAnimation from './src/Screens/AboutAnimation/AboutAnimation';
+
+const App = () => {
+  return <AboutAnimation />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/Screens/AboutAnimation/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+export default styles = StyleSheet.create(({
+    mainContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageStyle: {
+        height: 100, width: 100, backgroundColor: 'red' 
+    }
+}))
